de60fefd68a058b1f4cc15714215155104a0c6db
misaka10987 <136697326+misaka10987@users.noreply.github.com>
2026-07-27 18:12:06 +0800

    remove comments

--- userscript.config.ts
+++ userscript.config.ts
@@ -4,8 +4,4 @@ export default defineConfig({
   id: 'bun-userscript-template',
   name: 'Bun Userscript Template',
   description: 'Template for building browser userscripts with Bun.',
-  
-  // meta: {
-  //   '@name': ['Bun Userscript Template'],
-  // },
 })
